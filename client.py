6730a1892839f683d4a6d8e2bba4d68c3915cca2
Oscar Buon <oscar.buon@surgar-surgery.com>
2025-04-14 13:29:21 +0200

    use canals

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ def set_fan_speed(gpu_id=0, fan_speed=80):
 def map_range(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def run_fan_client(server_ip, server_port):
+def run_fan_client(server_ip, server_port, canal):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((server_ip, server_port))
@@ -30,9 +30,9 @@ def run_fan_client(server_ip, server_port):
                 message = json.loads(buffer)
                 buffer = ""  # Reset buffer after full JSON
                 axes = message.get("axes", {})
-                if '0' in axes:
-                    val = -axes['0']
-                    fan_speed = map_range(val, -33000, 33000, 30, 100)
+                if f"{canal}" in axes:
+                    val = -axes[f"{canal}"]
+                    fan_speed = map_range(val, -33000, 33000, 0, 100)
                     print(f"🌀 Setting fan speed: {int(fan_speed)}%")
                     set_fan_speed(fan_speed=int(fan_speed))
     except KeyboardInterrupt:
@@ -46,6 +46,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fan control client.")
     parser.add_argument('--ip', type=str, default='localhost', help='Server IP address (default: localhost)')
     parser.add_argument('--port', type=int, default=9999, help='Server port (default: 9999)')
+    parser.add_argument('--canal', type=int, default=0, help='Canal (default: 0)')
     args = parser.parse_args()
 
-    run_fan_client(server_ip=args.ip, server_port=args.port)
+    run_fan_client(server_ip=args.ip, server_port=args.port, canal=args.canal)
